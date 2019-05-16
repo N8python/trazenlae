af643514754b51d247c0910991f938e19b804d96
n8programs <43304488+N8python@users.noreply.github.com>
2019-05-16 15:46:37 -0400

    Create trazenlae.js

---
+++ trazenlae.js
@@ -0,0 +1,10 @@
+ async function Translatr(jsonlink){
+  const response = await fetch(jsonlink);
+  const json = await response.json();
+  return {
+    langify(lang) {
+      for(const [key, value] of Object.entries(json[lang])){
+        document.querySelector(key).innerHTML = value;
+      }
+    }
+  }
